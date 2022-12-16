d13b6672e597f5f4ad6641ccff14e89e0fa9b919
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-12-16 21:03:03 +0100

    add changes

--- server.js
+++ server.js
@@ -1,35 +1,42 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('.hbs',hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
+
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
+
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info');
 });
 
-app.get('/history', (req, res, next) => {
-  res.show('history.html');
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello',{ name: `${req.params.name}`})
 });
 
 app.use((req, res) => {
@@ -39,4 +46,5 @@ app.use((req, res) => {
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
+
 
