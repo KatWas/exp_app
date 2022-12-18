99bdaa6b206b12f00fee6c8dd97f5ed2431b58ac
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-12-18 21:23:59 +0100

    add new

--- server.js
+++ server.js
@@ -1,48 +1,44 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 
 const app = express();
 
 app.engine('.hbs',hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
-app.set('view engine', '.hbs');
+app.set('view engine', '.hbs')
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, '/public')));
-
-app.post('/contact/send-message', (req, res) => {
-
-  const { author, sender, title, message } = req.body;
-
-  if(author && sender && title && message) {
-    res.render('contact', { isSent: true });
-  }
-  else {
-    res.render('contact', { isError: true });
-  }
-
+app.post('/contact/send-message', upload.single('myFile'), (req, res) => {
+  const { author, sender, title, message, myFile} = req.body;
+  const fileName = req.file.originalname;
+  console.log('originalname: ', fileName)
+  if(author && sender && title && message && fileName) {
+    res.render('contact', { isSent : true, name1: fileName});
+  } else { res.render( 'contact' , { isError: true} )}
 });
 
-app.get('/hello/:name', (req, res) => {
-  res.render('hello', { name: req.params.name });
-});
+app.get( '/hello/:name' , (req, res) => {
+  res.render('hello', { name: req.params.name})
+})
 
-app.get('/', (req, res) => {
+app.get( '/', (req, res)=>{
   res.render('index');
 });
 
-app.get('/about', (req, res) => {
+app.get('/about', ( req,res ) => {
   res.render('about');
 });
 
-app.post('/contact', (req, res) => {
-  res.json(req.body);
+app.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
-
 app.get('/info', (req, res) => {
-  res.render('info');
+  res.render('info', { layout: 'dark'});
 });
 
 app.get('/history', (req, res) => {
@@ -53,12 +49,10 @@ app.get('/hello/:name', (req, res) => {
   res.render('hello',{ name: `${req.params.name}`})
 });
 
-app.use((req, res) => {
-  res.status(404).send('404 not found...');
-})
+app.use(( req, res ) => {
+  res.status(400).send('404 not found...');
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
-
-
