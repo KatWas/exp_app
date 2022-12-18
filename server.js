5db0b69f18138d369a8ebe0e432477f915c36a3b
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-12-18 21:28:36 +0100

    add changes

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.get('/about', ( req,res ) => {
   res.render('about');
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact');
+app.post('/contact', (req, res) => {
+  res.json(req.body);
 });
 
 app.get('/info', (req, res) => {
